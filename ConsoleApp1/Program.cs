79a71cefabf10abbbb0b0e3b65057a678b782598
Juan Tomás Arrativel <arrativeltomas@gmail.com>
2023-10-23 10:35:50 -0300

    Bool, integer, long and decimals

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,9 +6,21 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.Write("What is your name? ");
-            string name = Console.ReadLine();
-            Console.WriteLine($"Hello {name}");
+            bool canIVote = true;
+
+            Console.WriteLine("Biggest Integer : {0}", int.MaxValue);
+            Console.WriteLine("Smallest integer : {0}", int.MinValue);
+
+            Console.WriteLine("biggest long : {0}", long.MaxValue);
+            Console.WriteLine("Smallest long : {0}", long.MinValue);
+
+            decimal decPiVal = 3.14159265M;
+            decimal decBigNum = 3.0000000000000000000000011M;
+            Console.WriteLine("DEC : PI + bigNum = {0}", decPiVal + decBigNum);
+
+            Console.WriteLine("bigst decim: {0}", decimal.MaxValue);
+            Console.WriteLine("small decim: {0}", decimal.MinValue);
+
         }
     }
 }
