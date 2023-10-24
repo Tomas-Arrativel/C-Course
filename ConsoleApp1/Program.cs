872cf9b4e932a2f87dc1fd17b7bd4e2d6c2a99f2
Juan Tomás Arrativel <arrativeltomas@gmail.com>
2023-10-24 09:59:07 -0300

    Arrays and for Loop

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,28 +6,24 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            string randString = "This is a string";
-            Console.WriteLine("String Length : {0}", randString.Length);
-            Console.WriteLine("String contains is : {0}", randString.Contains("is"));
-            Console.WriteLine("Index of is : {0}", randString.IndexOf("is"));
-            Console.WriteLine("Remove string : {0}", randString.Remove(10, 6));
-            Console.WriteLine("Insert String : {0}", randString.Insert(10, "Short "));
-            Console.WriteLine("Replace string : {0}", randString.Replace("string", "sentence"));
-            Console.WriteLine("Compare String : {0}", String.Compare("A", "B", StringComparison.OrdinalIgnoreCase));
-            Console.WriteLine("----------------------------");
-           
-            Console.WriteLine("A = a : {0}", String.Equals("A", "a", StringComparison.OrdinalIgnoreCase));
-            Console.WriteLine("Pad left : {0}", randString.PadLeft(20, '.'));
-            Console.WriteLine("Pad right : {0}", randString.PadRight(20, '.'));
-            Console.WriteLine("Trim : {0}", randString.Trim());
-            Console.WriteLine("Uppercase : {0}", randString.ToUpper());
-            Console.WriteLine("Lowercase : {0}", randString.ToLower());
-
-            string newString = String.Format("{0} saw a {1} {2} in the {3}",
-                "Paul", "white", "rabbit", "bathroom");
-            Console.Write(newString + "\n");
-            // \' \" \\ \t \a
-            Console.WriteLine(@"Exactly what I Typed\n");
+            int[] favNums = new int[3];
+            favNums[0] = 23;
+            Console.WriteLine("favNum 0 : {0}", favNums[0]);
+            string[] customers = { "Bob", "Alice", "Sue" };
+            var employees = new[] { "Mike", "Paul", "Rick" };
+            object[] randomArray = { "paul", 45, 1.234 };
+            Console.WriteLine("randomArray 0 : {0}", randomArray[0].GetType());
+            Console.WriteLine("Array Size : {0}", randomArray.Length);
+            for (int i = 0; i < randomArray.Length; i++)
+            {
+                Console.WriteLine("Array : {0} : Value : {1}", i, randomArray[i]);
+            }
+            Console.WriteLine("-------------------");
+            string[,] custName = new string[2, 2] { { "Bob", "Smaith" }, { "Sally", "Smith" } };
+            Console.WriteLine("MD Value : {0}", custName.GetValue(0, 1));
+            for (int k = 0; k < custName.GetLength(0); k++) {
+                
+            }
         }
     }
 }
