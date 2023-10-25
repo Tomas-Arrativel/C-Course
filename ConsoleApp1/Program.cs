5c7403f6e5e8e5097db6e1f1a3b4afe2210c3cca
Juan Tomás Arrativel <arrativeltomas@gmail.com>
2023-10-25 11:16:24 -0300

    while loop

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -18,53 +18,18 @@ namespace ConsoleApp1
 
         static void Main(string[] args)
         {
-            // Relational Operators : < > >= <= == !=
-            // Logical Operators : && || !
-
-            int age = 17;
-
-            if ((age >= 5) && (age <= 7))
+            int i = 1;
+            while (i <= 10)
             {
-                Console.WriteLine("Go to elementary school");
+                if(i % 2 == 0)
+                {
+                    i++;
+                    continue;
+                }
+                if (i == 9) break;
+                Console.WriteLine(i);
+                i++;
             }
-            if ((age > 7) && (age < 13))
-            {
-                Console.WriteLine("Go to middle school");
-            }
-            if ((age > 13) && (age < 19))
-            {
-                Console.WriteLine("Go to high school");
-            }
-            else { Console.WriteLine("Go to college"); }
-
-            if ((age < 14) || (age > 67))
-            {
-                Console.WriteLine("You shouldn't work");
-            }
-            Console.WriteLine("! true = " + (!true));
-
-            bool canDrive = age >= 16 ? true : false;
-
-            switch (age)
-            {
-                case 1:
-                case 2:
-                    Console.WriteLine("Go to day care");
-                    break;
-                case 3:
-                case 4:
-                    Console.WriteLine("Go to preschool");
-                    break;
-                case 5:
-                    Console.WriteLine("Go to Kindergarten");
-                    break;
-                default:
-                    Console.WriteLine("Go to another school");
-                    goto OtherSchool;
-            }
-
-        OtherSchool:
-            Console.WriteLine("Elementary, Middle, High school");
         }
     }
 }
