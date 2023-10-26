bd7174565da8ff6808292fcaf258ff559919faa0
Juan Tomás Arrativel <arrativeltomas@gmail.com>
2023-10-26 11:21:59 -0300

    Swap function pass by reference

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -14,18 +14,23 @@ namespace ConsoleApp1
         // Private: Can't be accessed from another cl
         // Protected: Can't be accessed by class but can be by derived classes
        
-        static void DoubleIt(int x, out int solution)
+        public static void Swap(ref int num3, ref int num4)
         {
-            solution = x * 2;
+            int temp = num3;
+            num3 = num4;
+            num4 = temp;
         }
 
         // ------ END OF FUNCTIONS ------
 
         static void Main(string[] args)
         {
-            int solution;
-            DoubleIt(15, out solution);
-            Console.WriteLine("15 * 2 = {0}", solution);
+            int num3 = 10;
+            int num4 = 20;
+            Console.WriteLine("Before Swap num1 : {0} num2 : {1}", num3, num4);
+            Swap(ref num3, ref num4);
+            Console.WriteLine("After Swap num1 : {0} num2 : {1}", num3, num4);
+
         }
     }
 }
