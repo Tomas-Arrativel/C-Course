175506e43630b591cddce10c3d284fe5a22daa72
Juan Tomás Arrativel <arrativeltomas@gmail.com>
2023-10-26 10:01:08 -0300

    Do while loop with random number

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -18,18 +18,18 @@ namespace ConsoleApp1
 
         static void Main(string[] args)
         {
-            int i = 1;
-            while (i <= 10)
+            Random rnd = new Random();
+            int secretNumber = rnd.Next(1, 11);
+            int numberGuessed = 0;
+            Console.WriteLine("Random num : ", secretNumber);
+
+            do
             {
-                if(i % 2 == 0)
-                {
-                    i++;
-                    continue;
-                }
-                if (i == 9) break;
-                Console.WriteLine(i);
-                i++;
-            }
+                Console.WriteLine("Enter a number between 1 & 10");
+                numberGuessed = Convert.ToInt32(Console.ReadLine());
+            } while (secretNumber != numberGuessed);
+
+            Console.WriteLine("You guessed it, secret number was {0}", secretNumber);
         }
     }
 }
