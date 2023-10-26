fd2c88ee7c74fb58d2178940c7b59a405149f3c0
Juan Tomás Arrativel <arrativeltomas@gmail.com>
2023-10-26 11:03:13 -0300

    private function

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -7,46 +7,26 @@ namespace ConsoleApp1
     public class Program
     {
         // ------ FUNCTIONS ------
-
-        static void PrintArray(int[] intArray, string mess)
+        // <Access Specifier> <Return Type> <Method Name>(Parameters)
+        // { <Body> }
+        // Access Specifier determines whether the function can be called from another class
+        // Public: Can be accessed from another cl
+        // Private: Can't be accessed from another cl
+        // Protected: Can't be accessed by class but can be by derived classes
+       
+        private static void SayHello()
         {
-            foreach (int k in intArray)
-            {
-                Console.WriteLine("{0} : {1}", mess, k);
-            }
-        }
-
-        static double DoDivision(double x, double y)
-        {
-            if (y == 0)
-            {
-                throw new System.DivideByZeroException();
-            }
-            return x / y;
+            string name = "";
+            Console.Write("What is your name : ");
+            name = Console.ReadLine();
+            Console.WriteLine("Hello {0}", name);
         }
 
         // ------ END OF FUNCTIONS ------
 
         static void Main(string[] args)
         {
-            StringBuilder sb = new StringBuilder("Random Text");
-            StringBuilder sb2 = new StringBuilder("More stuff that is very important", 256);
-            Console.WriteLine("Capacity : {0}", sb2.Capacity);
-            Console.WriteLine("Length : {0}", sb2.Length);
-            sb2.AppendLine("\nMore important text");
-            CultureInfo enUS = CultureInfo.CreateSpecificCulture("en-US");
-            string bestCust = "Bob Smith";
-            sb2.AppendFormat(enUS, "Best Customer : {0}", bestCust);
-            Console.WriteLine(sb2.ToString());
-            sb2.Replace("text", "characters");
-            Console.WriteLine(sb2.ToString());
-            sb2.Clear();
-            sb2.Append("Random text");
-            Console.WriteLine(sb.Equals(sb2));
-            sb2.Insert(11, " that's great");
-            Console.WriteLine(sb2.ToString());
-            sb2.Remove(11, 7);
-            Console.WriteLine(sb2.ToString());
+            SayHello();
 
         }
     }
