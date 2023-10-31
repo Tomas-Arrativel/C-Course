38f60b1276627a00668487077e0c2d5395891a2f
Juan Tomás Arrativel <arrativeltomas@gmail.com>
2023-10-31 09:51:11 -0300

    structs

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -20,7 +20,26 @@ namespace ConsoleApp1
 
         static void Main(string[] args)
         {
-            
+            Rectangle rect1;
+            rect1.length = 200;
+            rect1.width = 500;
+            Console.WriteLine("Area of rect1 : {0}", rect1.Area());
+            Rectangle rect2 = new Rectangle(100, 40);
+            rect2 = rect1;
+            rect1.length = 33;
+            Console.WriteLine("rect2.length : {0}", rect2.length);
+        }
+
+        struct Rectangle
+        {
+            public double length;
+            public double width;
+
+            public Rectangle(double l = 1, double w = 1) { length = l; width = w; }
+            public double Area()
+            {
+                return length * width;
+            }
         }
     }
 }
