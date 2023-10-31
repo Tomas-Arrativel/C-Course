0e238f9a070a0ed712e602b491a120a11188e5ff
Juan Tomás Arrativel <arrativeltomas@gmail.com>
2023-10-31 09:02:01 -0300

    date time / timespan

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -14,25 +14,22 @@ namespace ConsoleApp1
         // Private: Can't be accessed from another cl
         // Protected: Can't be accessed by class but can be by derived classes
 
-        static double GetSum2(double x = 1, double y = 1)
-        {
-            return x + y;
-        }
-        static double GetSum2(string x = "1", string y = "1")
-        {
-            double dblX = Convert.ToDouble(x);
-            double dblY = Convert.ToDouble(y);
-            return dblX + dblY;
-        }
-
-
+        
 
         // ------ END OF FUNCTIONS ------
 
         static void Main(string[] args)
         {
-            Console.WriteLine("5.0 + 4.5 = {0} double", GetSum2(5.0, 4.5));
-            Console.WriteLine("5.0 + 4.5 = {0} string", GetSum2("5.0", "4.5"));
+            DateTime awesomeDate = new DateTime(2004, 9, 5);
+            Console.WriteLine("Day of the week {0}", awesomeDate.DayOfWeek);
+            awesomeDate = awesomeDate.AddDays(6);
+            awesomeDate = awesomeDate.AddMonths(1);
+            awesomeDate = awesomeDate.AddYears(2);
+            Console.WriteLine("New Date : {0}", awesomeDate.Date);
+
+            TimeSpan lunchTime = new TimeSpan(13, 30, 0);
+            lunchTime = lunchTime.Subtract(new TimeSpan(0, 15, 0));
+            Console.WriteLine("New time : {0}", lunchTime.ToString());
         }
     }
 }
