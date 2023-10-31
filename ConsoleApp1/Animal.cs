4fb8f923d449d18de38c0d603da49207e1ee0caf
Juan Tomás Arrativel <arrativeltomas@gmail.com>
2023-10-31 10:27:31 -0300

    Basic setter / getter, readonly, constants, public/private/protected

--- ConsoleApp1/Animal.cs
+++ ConsoleApp1/Animal.cs
@@ -8,29 +8,24 @@ namespace ConsoleApp1
 {
     class Animal
     {
-        public string name;
-        public string sound;
-        static int numOfAnimals = 0;
+        private string name;
+        private string sound;
+        public static int numOfAnimals = 0;
+        public const string SHELTER = "Tomi's Home for animals";
+        public readonly int idNum;
 
-        public Animal()
-        {
-            name = "No Name";
-            sound = "No Sound";
-            numOfAnimals++;
-        }
+        public Animal() : this("No Name", "No Sound") { }
 
-        public Animal(string name = "No Name")
-        {
-            this.name = name;
-            this.sound = "No Sound";
-            numOfAnimals++;
-        }
+        public Animal(string name) : this(name, "No Sound") { }
 
-        public Animal(string name = "No Name", string sound = "No Sound")
+        public Animal(string name, string sound)
         {
-            this.name = name;
-            this.sound = sound;
-            numOfAnimals++;
+            SetName(name);
+            Sound = sound;
+
+            NumOfAnimals = 1;
+            Random rnd = new Random();
+            idNum = rnd.Next(1, 2147483640);
         }
 
         public void MakeSound()
@@ -42,5 +37,45 @@ namespace ConsoleApp1
             return numOfAnimals;
         }
 
+        public void SetName(string name)
+        {
+            if(!name.Any(char.IsDigit))
+            {
+                this.name = name;
+            } else
+            {
+                this.name = "No Name";
+                Console.WriteLine("Name can't contain numbers");
+            }
+        }
+
+        public string GetName()
+        {
+            return name;
+        }
+
+        public string Sound
+        {
+            get { return sound; }
+            set
+            {
+                if(value.Length > 10)
+                {
+                    sound = "No Sound";
+                    Console.WriteLine("Sound is too long");
+                } else
+                {
+                    sound = value;
+                }
+            }
+        }
+
+        public string Owner { get; set; } = "No Owner";
+
+        public static int NumOfAnimals
+        {
+            get { return numOfAnimals; }
+            set { numOfAnimals += value; }
+        }
     }
 }
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -14,46 +14,20 @@ namespace ConsoleApp1
         // Private: Can't be accessed from another cl
         // Protected: Can't be accessed by class but can be by derived classes
 
-        
+
 
         // ------ END OF FUNCTIONS ------
 
         static void Main(string[] args)
         {
-            Rectangle rect1;
-            rect1.length = 200;
-            rect1.width = 500;
-            Console.WriteLine("Area of rect1 : {0}", rect1.Area());
-            Rectangle rect2 = new Rectangle(100, 40);
-            rect2 = rect1;
-            rect1.length = 33;
-            Console.WriteLine("rect2.length : {0}", rect2.length);
-
-            Animal fox = new Animal()
-            {
-                name = "Red",
-                sound = "Raaaaw"
-            };
-
-            Console.WriteLine("# of Animals {0}", Animal.GetNumAnimals());
-
-            Console.WriteLine("Area of Rectangle : {0}", ShapeMath.GetArea("Rectangle", 5, 6));
-
-            int? randNum = null;
-            if(randNum == null) { Console.WriteLine("randNum is Null"); }
-            if(!randNum.HasValue) { Console.WriteLine("randNum is null"); }
-        }
-
-        struct Rectangle
-        {
-            public double length;
-            public double width;
-
-            public Rectangle(double l = 1, double w = 1) { length = l; width = w; }
-            public double Area()
-            {
-                return length * width;
-            }
+            Animal cat = new Animal();
+            cat.SetName("Whiskers");
+            cat.Sound = "Meow";
+            Console.WriteLine("The cat is named {0} and says {1}", cat.GetName(), cat.Sound);
+            cat.Owner = "Tomás";
+            Console.WriteLine("{0} owner is {1}", cat.GetName(), cat.Owner);
+            Console.WriteLine("{0} shelter id is {1}", cat.GetName(), cat.idNum);
+            Console.WriteLine("# of animals : {0}", Animal.NumOfAnimals);
         }
     }
 }
