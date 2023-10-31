8232b7069da357bd6f1cca69a1527fb109f6aab7
Juan Tomás Arrativel <arrativeltomas@gmail.com>
2023-10-31 09:32:03 -0300

    Classes / Objects

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -14,26 +14,13 @@ namespace ConsoleApp1
         // Private: Can't be accessed from another cl
         // Protected: Can't be accessed by class but can be by derived classes
 
-        static void PaintCar(CarColor cc)
-        {
-            Console.WriteLine("The car was painted {0}, with the code {1}", cc, (int)cc);
-        }
+        
 
         // ------ END OF FUNCTIONS ------
 
-        enum CarColor : byte
-        {
-            Orange = 1,
-            Blue,
-            Green,
-            Red,
-            Yellow
-        }
-
         static void Main(string[] args)
         {
-            CarColor car1 = CarColor.Blue;
-            PaintCar(car1);
+            
         }
     }
 }
